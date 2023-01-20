10f6db8e4556b02216f61aa58407a8be446c710a
Daniel Gaull <gaulldj@rose-hulman.edu>
2023-01-19 20:15:10 -0500

    Added create scripts for Login and Session tables

---
+++ SQL Scripts/create_tables_script_3.sql
@@ -0,0 +1,22 @@
+USE TeamXCDB
+GO
+
+-- Login and Session tables
+
+CREATE TABLE [Login](
+	email varchar(100) PRIMARY KEY NOT NULL,
+	password_hash varchar(50) NOT NULL,
+	password_salt varchar(50) NOT NULL,
+	created_at datetime NOT NULL,
+	last_login datetime NOT NULL,
+
+	CHECK(email LIKE '%@%.%')
+)
+
+CREATE TABLE [Session](
+	session_id uniqueidentifier PRIMARY KEY NOT NULL DEFAULT NEWID(),
+	expires_at datetime NOT NULL,
+	user_email varchar(100) NOT NULL,
+
+	FOREIGN KEY(user_email) REFERENCES [Login](email)
+)
