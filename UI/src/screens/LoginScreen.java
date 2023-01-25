0ac11c1a272504b4163402812fc2184aac221f74
Daniel Gaull <gaulldj@rose-hulman.edu>
2023-01-25 16:19:45 -0500

    Added Screen and start of login system

---
+++ UI/src/screens/LoginScreen.java
@@ -0,0 +1,44 @@
+package screens;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class LoginScreen extends Screen {
+
+    private JTextField usernameBox;
+    private JTextField passwordBox;
+
+    private JLabel errorLabel;
+
+    public LoginScreen() {
+    }
+
+    @Override
+    public void populatePanel() {
+        // Parent panel has 2 rows and 1 column (we'll have 2 panels that go inside of it)
+        super.createPanel(2, 1);
+        JPanel parent = super.getPanel();
+        // Create our panels
+        JPanel top = new JPanel(); // 2 rows (user, pass), 2 cols (label, field entry)
+        top.setLayout(new GridLayout(2, 2));
+        JPanel bottom = new JPanel(); // 2 rows (error message, login button), 1 col
+        bottom.setLayout(new GridLayout(2, 1));
+        parent.add(top);
+        parent.add(bottom);
+
+        // Populate the top panel
+        this.usernameBox = new JTextField();
+        this.passwordBox = new JPasswordField();
+        top.add(new JLabel("Username: "));
+        top.add(this.usernameBox);
+        top.add(new JLabel("Password: "));
+        top.add(this.passwordBox);
+
+        // Populate bottom panel
+        JButton loginButton = new JButton("Login");
+        this.errorLabel = new JLabel("");
+        this.errorLabel.setForeground(Color.RED);
+        bottom.add(errorLabel);
+        bottom.add(loginButton);
+    }
+}
---
+++ UI/src/screens/Screen.java
@@ -0,0 +1,21 @@
+package screens;
+
+import javax.swing.*;
+import java.awt.*;
+
+public abstract class Screen {
+
+    private JPanel panel;
+
+    public abstract void populatePanel();
+
+    public void createPanel(int rows, int cols) {
+        GridLayout layout = new GridLayout(rows, cols);
+        panel = new JPanel();
+        panel.setLayout(layout);
+    }
+
+    public JPanel getPanel() {
+        return panel;
+    }
+}
