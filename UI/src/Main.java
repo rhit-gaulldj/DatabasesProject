0dd4eb98ae684be52b7e0b5dbd255114e14c534b
Daniel Gaull <gaulldj@rose-hulman.edu>
2023-01-24 20:58:42 -0500

    Add database connection service

---
+++ UI/src/Main.java
@@ -0,0 +1,15 @@
+import javax.swing.*;
+
+public class Main {
+
+    public static void main(String[] args) {
+        new Main();
+    }
+
+    public Main() {
+        JFrame frame = new JFrame("Cross Country App");
+
+
+        frame.setVisible(true);
+    }
+}
