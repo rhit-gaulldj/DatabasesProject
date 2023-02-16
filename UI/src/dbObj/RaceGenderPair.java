ea9c4849763d290821fda33f6aebed631ca3fd18
Daniel Gaull <gaulldj@rose-hulman.edu>
2023-02-15 19:41:49 -0500

    Cleanup

--- UI/src/dbObj/RaceGenderPair.java
+++
@@ -1,8 +0,0 @@
-package dbObj;
-
-public record RaceGenderPair(RaceLevel level, Gender gender) {
-    @Override
-    public String toString() {
-        return gender.toLongString() + " " + level.toString();
-    }
-}
